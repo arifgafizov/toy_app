ceed3a86fe8d1ce03014a96fa8c0308ba5d22488
Arif <agafizov@gmail.com>
2020-02-13 22:20:21 +0300

    Add micropost scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # для отображения 'users#index' на url localhost:3000
+  root 'users#index'
 end
